a5c0731da9ceccb2cf96efbea4110ead64cc3dae
imthur <arthur.aguiar8708@gmail.com>
2025-04-03 12:09:14 -0300

    Adicionando funções de exportar para PDF e Excel

--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from .models import transacao
 from .serializers import TransacaoSerializer
 from django.shortcuts import render
+import pandas as pd
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 
 class TransacaoViewSet(viewsets.ModelViewSet):
     queryset = transacao.objects.all()
@@ -9,3 +12,19 @@ class TransacaoViewSet(viewsets.ModelViewSet):
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def exportar_excel(request):
+    transacoes = transacao.objects.all().values()
+    df = pdf.DataFrame(transacoes)
+    response = HttpResponse(content_type='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="relatorio.xlsx"'
+    df.to_excel(response,index=False)
+    return response
+
+def exportar_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="relatorio.pdf"'
+    pdf = canvas.Canvas(response)
+    pdf.drawString(100, 800, "Relatório Financeiro")
+    pdf.save()
+    return response
--- financeiro/urls.py
+++ financeiro/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import TransacaoViewSet, dashboard
+from core.views import TransacaoViewSet, dashboard, exportar_excel, exportar_pdf
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -8,5 +8,7 @@ router.register('transacoes', TransacaoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
+    path('exportar_excel/', exportar_excel, name='exportar_excel'),
+    path('exportar_pdf/', exportar_pdf, name='exportar_pdf')
 ]
